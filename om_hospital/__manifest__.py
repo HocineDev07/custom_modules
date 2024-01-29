dc36a9cd71f67ae1003916bfb0d73fd7ab6d98b7
HocineDev07 <houcinehoud664@gmail.com>
2024-01-29 08:24:20 +0100

    change version

--- om_hospital/__manifest__.py
+++ om_hospital/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Odoo 16 Development Tutorials',
-    'version': '2.0.0',
+    'version': '2.0.1',
     'summary': 'Odoo 16 Development Tutorials',
     'sequence': -100,
     'description': """Odoo 16 Development Tutorials""",
